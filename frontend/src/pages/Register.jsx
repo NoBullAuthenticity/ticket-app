1468af8be67c7600258208d61425f809a64cdf62
NoBullAuthenticity <nobullsecureyourauthenticity@gmail.com>
2022-03-20 16:03:38 -0400

    halfway through Register user

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {useState} from 'react'
 import {FaUser} from 'react-icons/fa'
 import {toast} from 'react-toastify'
+import {useSelector, useDispatch} from 'react-redux'
+import {register} from '../features/auth/authSlice'
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -14,6 +16,10 @@ function Register() {
   })
 
   const {name, email, password, password2} = formData
+
+  const dispatch = useDispatch()
+
+  const {user, isError, isLoading, isSuccess, message} = useSelector(state => state.auth)
 
   const onChange = (e) => {
 
@@ -29,11 +35,20 @@ function Register() {
 
     e.preventDefault()
 
-    
-
     if (password !== password2) {
 
         toast.error('passwords do not match')
+
+    } else {
+
+        const userData = {
+
+            name, 
+            email, 
+            password
+        }
+
+        dispatch(register(userData))
 
     }
 
@@ -44,7 +59,7 @@ function Register() {
     <>
       <section className='heading'>
         <h1>
-          <FaUser /> Register
+          <FaUser /> Register {user}
         </h1>
         <p>Please create an account</p>
       </section>
