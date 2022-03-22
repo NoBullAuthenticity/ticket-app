60b6268bf4cc81c44f49b103ee175bfa9b4c63b3
NoBullAuthenticity <nobullsecureyourauthenticity@gmail.com>
2022-03-21 22:16:57 -0400

    lesson 140, tickets no redux

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import {FaSignInAlt, FaUser} from 'react-icons/fa'
 import {useSelector, useDispatch} from 'react-redux'
-import {login} from '../features/auth/authSlice'
+import { useNavigate } from 'react-router-dom'
+import {login, reset} from '../features/auth/authSlice'
 import {toast} from 'react-toastify'
+import Spinner from '../components/Spinner'
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -17,8 +19,22 @@ function Login() {
   const { email, password } = formData
 
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const {user, isError, isLoading, isSuccess, message} = useSelector(state => state.auth)
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    // Redirect when logged in
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [isError, isSuccess, user, message, navigate, dispatch])
 
   const onChange = (e) => {
 
@@ -45,6 +61,9 @@ function Login() {
 
   }
 
+  if (isLoading) {
+    return <Spinner />
+  }
 
   return (
     <>
